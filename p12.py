4b79ee1e6e90ec28ba190e5317e1ded33b983351
masxman <118148063+masxman@users.noreply.github.com>
2024-01-04 21:58:13 +0530

    Create p12.py

---
+++ p12.py
@@ -0,0 +1,12 @@
+import numpy as np
+arr=np.array([1,2,3,4,5])
+print(arr)
+print(type(arr))
+nparray=np.array([[1,2,3],[11,22,33],[4,5,6],[8,9,10],[20,30,40]])
+print(nparray)
+output=nparray.sum(axis=1)
+print("\n\nSum column-wise:",output)
+arr=np.array([[1,2,3,4],[5,6,7,8]])
+print(arr.shape) 
+arr=np.array([1,2,3,4])
+print(arr.dtype) 
