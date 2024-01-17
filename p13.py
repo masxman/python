74ef149c9cdf8d75c7bf650a106cb3a19d5852fa
masxman <118148063+masxman@users.noreply.github.com>
2024-01-17 14:13:06 +0530

    concat data frames

--- p13.py
+++ p13.py
@@ -4,3 +4,4 @@ df1=pd.DataFrame({'Name':['Tommy','Fury'],'age':[20,21]},index=[1,2])
 #create second Datafram
 df2=pd.DataFrame({'Name':['Jake','Paul'],'age':[22,3]},index=[3,4])
 print(pd.concat([df1,df2])) 
+#
