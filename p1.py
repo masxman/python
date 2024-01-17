d21f648d39f12d1a4cf46118b3aa20988f58e538
masxman <118148063+masxman@users.noreply.github.com>
2024-01-17 13:57:05 +0530

    data types

---
+++ p1.py
@@ -0,0 +1,35 @@
+a = 20 #int
+print(a)
+print(type(a))
+
+b = 20.5 #float
+print(b)
+print(type(b))
+
+c = 1j #complex
+print(c)
+print(type(c))
+
+d = "hello world" #String
+print(d)
+print(type(d))
+
+e = ["Python", "OS", "CN"] #list
+print(e)
+print(type(e))
+
+f = ("Python", "CN", "OS") #tuple
+print(f)
+print(type(f))
+
+g = {"name" : "anu", "age" : 36} #dict
+print(g)
+print(type(g))
+
+h = True #bool
+print(h)
+print(type(h))
+
+i = b"Hello" #bytes
+print(i)
+print(type(i))
