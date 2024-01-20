eb6580d514feef94d5857d9c8a00da3f1010329d
masxman <118148063+masxman@users.noreply.github.com>
2024-01-20 08:46:34 +0530

    array

--- p12.py
+++ p12.py
@@ -1,13 +1,22 @@
 import numpy as np
-arr=np.array([1,2,3,4,5])
+
+# Creating a 1D array
+arr = np.array([1,2,3,4,5])
 print(arr)
 print(type(arr))
-nparray=np.array([[1,2,3],[11,22,33],[4,5,6],[8,9,10],[20,30,40]])
+
+# Creating a 2D array
+nparray = np.array([[1,2,3],[11,22,33],[4,5,6],[8,9,10],[20,30,40]])
 print(nparray)
-output=nparray.sum(axis=1)
-print("\n\nSum column-wise:",output)
-arr=np.array([[1,2,3,4],[5,6,7,8]])
-print(arr.shape) 
-arr=np.array([1,2,3,4])
-print(arr.dtype) 
-#
+
+# Summing column-wise
+output = nparray.sum(axis=1)
+print("\n\nSum column-wise:", output)
+
+# Checking the shape of an array
+arr = np.array([[1,2,3,4],[5,6,7,8]])
+print(arr.shape)
+
+# Checking the data type of an array
+arr = np.array([1,2,3,4])
+print(arr.dtype)
